697d385d24532088ef4d42632f38f898c9e65042
marseljaber <155525563+marseljaber@users.noreply.github.com>
2025-09-03 15:57:46 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/4girls/">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
